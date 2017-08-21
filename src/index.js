9407331fe586f87e8c83a6401e21a13730f5b8b8
Brad Le <brad.le444@gmail.com>
2017-08-21 15:26:58 +0200

    Add Routing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom' //For routing in URL address bar
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+<BrowserRouter><App /></BrowserRouter>, 
+document.getElementById('root')
+)
+
+// BrowserRouter wraps App component to listen to URL changes in address bar
