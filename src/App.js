692173f6e0a36a48b847a272a899b9ebeff3a699
Brad Le <brad.le444@gmail.com>
2017-09-14 16:45:34 -0700

    Add Book Search Component

--- src/App.js
+++ src/App.js
@@ -2,36 +2,25 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Link, Route } from 'react-router-dom'
+import SearchBooks from './SearchBooks'
+import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-
+    books: []
   }
-
+  componentDidMount() {
+    BooksAPI.getAll().then((books)=>{
+      this.setState({ books })
+    })
+  }
   render() {
     return (
       <div className="app">
-        <Route path="/addbook" render={()=>(
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/" className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+        <Route path="/search" render={()=>(
+          <SearchBooks 
+            books={this.state.books}
+          />
         )}/>
         <Route exact path="/" render={()=>(
           <div className="list-books">
@@ -192,7 +181,7 @@ class BooksApp extends React.Component {
             <div className="open-search">
               {/* change to Link for routing pages properly */}
               <Link 
-                to="/addbook"
+                to="/search"
                 >Add a book</Link> 
             </div>
           </div>
